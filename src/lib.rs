168e6ad0474c1f8eba6ce18293de1bd4e5446bca
Maximilian Goisser <goisser94@gmail.com>
2019-11-24 14:36:24 +0100

    Add support for bonds and ringbonds

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,10 @@ use nom::character::is_digit;
 use nom::combinator::map;
 use nom::combinator::map_res;
 use nom::combinator::opt;
+use nom::multi::count;
+use nom::multi::many0;
 use nom::sequence::delimited;
+use nom::sequence::preceded;
 use nom::sequence::tuple;
 use nom::IResult;
 
@@ -130,26 +133,91 @@ fn atom(input: &[u8]) -> IResult<&[u8], Atom> {
 #[derive(Debug, PartialEq, Eq)]
 pub struct BranchedAtom {
     atom: Atom,
-    // TODO: ring_bonds: Vec<RingBond>,
+    ring_bonds: Vec<RingBond>,
     // TODO: branches: Vec<Branch>,
 }
 
 fn branched_atom(input: &[u8]) -> IResult<&[u8], BranchedAtom> {
-    map(atom, |atom| BranchedAtom { atom })(input)
+    map(tuple((atom, many0(ring_bond))), |(atom, ring_bonds)| {
+        BranchedAtom { atom, ring_bonds }
+    })(input)
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Bond {
+    Single,
+    Double,
+    Triple,
+    Quadruple,
+    Aromatic,
+    Up,
+    Down,
+}
+
+fn raw_bond(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    alt((
+        tag(b"-"),
+        tag(b"="),
+        tag(b"#"),
+        tag(b"$"),
+        tag(b":"),
+        tag(b"/"),
+        tag(b"\\"),
+    ))(input)
+}
+
+fn bond(input: &[u8]) -> IResult<&[u8], Bond> {
+    map(raw_bond, |bnd: &[u8]| match bnd {
+        b"-" => Bond::Single,
+        b"=" => Bond::Double,
+        b"#" => Bond::Triple,
+        b"$" => Bond::Quadruple,
+        b":" => Bond::Aromatic,
+        b"/" => Bond::Up,
+        b"\\" => Bond::Down,
+        _ => unreachable!(),
+    })(input)
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct RingBond {
+    bond: Option<Bond>,
+    ring_number: u8,
+}
+
+fn bond_digits(input: &[u8]) -> IResult<&[u8], u8> {
+    map_res(
+        map_res(
+            alt((
+                take_while_m_n(1, 1, is_digit),
+                preceded(tag(b"%"), take_while_m_n(2, 2, is_digit)),
+            )),
+            |s: &[u8]| std::str::from_utf8(s),
+        ),
+        |s: &str| s.parse::<u8>(),
+    )(input)
+}
+
+fn ring_bond(input: &[u8]) -> IResult<&[u8], RingBond> {
+    map(tuple((opt(bond), bond_digits)), |(bond, ring_number)| {
+        RingBond { bond, ring_number }
+    })(input)
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Chain {
     chain: Option<Box<Chain>>,
+    bond: Option<Bond>,
     branched_atom: BranchedAtom,
     // TODO: other fields
 }
 
 fn chain(input: &[u8]) -> IResult<&[u8], Chain> {
     map(
-        tuple((branched_atom, opt(chain))),
-        |(branched_atom, chain)| Chain {
+        tuple((branched_atom, opt(bond), opt(chain))),
+        |(branched_atom, bond, chain)| Chain {
             chain: chain.map(|n| Box::new(n)),
+            bond,
             branched_atom,
         },
     )(input)
@@ -201,6 +269,13 @@ mod tests {
     }
 
     #[test]
+    fn ring_bond_digit_cases() {
+        assert_eq!(Ok(("".as_bytes(), 0u8)), bond_digits(b"0"));
+        assert_eq!(Ok(("".as_bytes(), 12u8)), bond_digits(b"%12"));
+        assert_eq!(Ok(("5".as_bytes(), 12u8)), bond_digits(b"%125"));
+    }
+
+    #[test]
     fn atom_cases() {
         assert_eq!(
             Ok((
@@ -223,16 +298,20 @@ mod tests {
                 Chain {
                     chain: Some(Box::new(Chain {
                         chain: None,
+                        bond: None,
                         branched_atom: BranchedAtom {
                             atom: Atom::AliphaticOrganic(AliphaticOrganicAtom {
                                 element: Element::Carbon
-                            })
+                            }),
+                            ring_bonds: vec![]
                         }
                     })),
+                    bond: None,
                     branched_atom: BranchedAtom {
                         atom: Atom::AliphaticOrganic(AliphaticOrganicAtom {
                             element: Element::Carbon
-                        })
+                        }),
+                        ring_bonds: vec![]
                     }
                 }
             )),
@@ -248,20 +327,71 @@ mod tests {
                 Chain {
                     chain: Some(Box::new(Chain {
                         chain: None,
+                        bond: None,
                         branched_atom: BranchedAtom {
                             atom: Atom::AliphaticOrganic(AliphaticOrganicAtom {
                                 element: Element::Fluorine
-                            })
+                            }),
+                            ring_bonds: vec![]
                         }
                     })),
+                    bond: None,
                     branched_atom: BranchedAtom {
                         atom: Atom::AliphaticOrganic(AliphaticOrganicAtom {
                             element: Element::Carbon
-                        })
+                        }),
+                        ring_bonds: vec![]
                     }
                 }
             )),
             chain(b"CF")
         );
+    }
+
+    #[test]
+    fn chain_ethene() {
+        assert_eq!(
+            Ok((
+                "".as_bytes(),
+                Chain {
+                    chain: Some(Box::new(Chain {
+                        chain: None,
+                        bond: None,
+                        branched_atom: BranchedAtom {
+                            atom: Atom::AliphaticOrganic(AliphaticOrganicAtom {
+                                element: Element::Carbon
+                            }),
+                            ring_bonds: vec![]
+                        }
+                    })),
+                    bond: Some(Bond::Double),
+                    branched_atom: BranchedAtom {
+                        atom: Atom::AliphaticOrganic(AliphaticOrganicAtom {
+                            element: Element::Carbon
+                        }),
+                        ring_bonds: vec![]
+                    }
+                }
+            )),
+            chain(b"C=C")
+        );
+    }
+
+    // 1-Oxaspiro[2.5]octane
+    #[test]
+    #[ignore]
+    fn ring_and_branch_chain() {
+        let chain = chain(b"C1CCC2(CC1)CO2");
+        assert!(chain.is_ok());
+        assert!(chain.unwrap().0.is_empty());
+    }
+
+    // Cyclopropyloxirane
+    #[test]
+    fn rings_chain() {
+        let chain = chain(b"C1CC1C2CO2");
+        println!("{:?}", chain);
+        assert!(chain.is_ok());
+        assert!(chain.unwrap().0.is_empty());
     }
 }
